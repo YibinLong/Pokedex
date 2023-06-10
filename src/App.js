a391234c0d996c40bd8696a6b56a3e2b19fd25fd
Yibin Long <yibinlong@outlook.com>
2023-06-09 20:49:21 -0400

    First commit - created basic Pokemon search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+function SearchBar({ onSearch }) {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSearch(searchTerm);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        placeholder="Search Pokemon..."
+        value={searchTerm}
+        onChange={(event) => setSearchTerm(event.target.value)} 
+      />
+      <button type="submit">Search</button>
+    </form>
+  )
 }
 
-export default App;
+const PokemonInfo = ({ pokemonName }) => {
+  const [pokemonData, setPokemonData] = useState(null);
+ 
+  useEffect(() => {
+    if (!pokemonName) return;
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then((response) => response.json())
+      .then((data) => setPokemonData(data));
+  }, [pokemonName]);
+
+  if (!pokemonData) {
+    return <div>Loading...</div>;
+  }
+
+  const name = pokemonData.name;
+  const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1);
+
+  return (
+    <div>
+      <h2 className='nice-font'>{nameCapitalized}</h2>
+      <img src={pokemonData.sprites.front_default} alt={nameCapitalized} />
+    </div>
+  )
+}
+
+function Pokedex() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (searchTerm) => {
+    setSearchTerm(searchTerm);
+  }
+
+  return (
+    <div>
+      <SearchBar onSearch={handleSearch}/>
+      <PokemonInfo pokemonName={searchTerm}/>
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <div className="centered-component">
+      <Pokedex />
+    </div>
+  ) 
+}
